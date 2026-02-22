9c8e34cfa7169b6b9e7913aa7a7640ef5619315c
Satyadarsh verma <satya12darsh@gmail.com>
2026-02-22 17:17:07 +0530

    first commit

---
+++ oop1.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def get_avg(self):
+        sum  = 0
+        for val in self.marks:
+            sum += val
+        
+        print("heloo" , self.name , "your avg score is " , sum/3)    
+
+
+
+s1 = Student("vivaan", [90,89,78])        
+s1.get_avg()
+        
+        
