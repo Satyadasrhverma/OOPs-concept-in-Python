79e2c886eb80551f0a9612672b82c47f759f5b5b
Satyadarsh verma <satya12darsh@gmail.com>
2026-02-23 02:21:49 +0530

    add overloading

---
+++ method_overloading/oop1.py
@@ -0,0 +1,9 @@
+##########--> while inheritance chile class and parent class have same method, method overriding is that same function but different owrk or output 
+#or when we call object of class there is childe class object perform, chile claass override parent methods 
+
+
+#####---> method overlaoding means in a same class we have tow methods with same but both are perform different logics(behaviour) according to input
+## it uses clan and readable code , but pthon doesnot support method overloading
+
+
+### Polymorphism -----1 overloading 2.overriding
